bbdd81c13c46144c3b53f7cfb0aae8be30388831
Olukeye <seunolukeye2000@gmail.com>
2021-09-24 10:25:04 +0100

    linked to the movie app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import React, { useState, useEffect } from 'react';
 import Topbar from './components/navbars/Topbar';
 import Sidebar from './components/sidebar/Sidebar';
 import Home from './Pages/Home';
 import "./app.css";
+import axios from 'axios'
 import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import {Users } from './Pages/usersPage/Users';
 import  EditUser  from './Pages/editusers/EditUser';
@@ -12,6 +14,40 @@ import CreateProducts  from './Pages/products/CreateProducts';
 
 
 function App() {
+  const [user, userState] = useState([]);
+
+  const  MONTHS = [
+    "Jan",
+    "Feb",
+    "March",
+    "April",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  useEffect(() => {
+    // get statistic of the users on the admin board
+    const getStat = async() => {
+      try{
+        const res = await axios.get("/user/state", {
+          header: {
+            token : ""
+          }
+        })
+        userState(res.data)
+      } catch(err) {
+        console.log(err)
+      } 
+    };
+    getStat()
+    }, [])
+
   return (
     <Router>
      <Topbar />
