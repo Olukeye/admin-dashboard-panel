838be751a74026d6e30a05e91bf98327e3dd605a
Olukeye <seunolukeye2000@gmail.com>
2021-10-12 19:26:50 +0100

    firebase implementation

--- src/Pages/products/CreateProducts.jsx
+++ src/Pages/products/CreateProducts.jsx
@@ -1,20 +1,58 @@
-import React from 'react'
-import './createproduct.css'
-import { useState } from 'react'
+import React from 'react';
+import './createproduct.css';
+import { useState } from 'react';
+import storage from '../../firebase';
 
 const CreateProducts = () => {
     const [ movie , setMovie] = useState(null);
     const [ img, setImg] = useState(null);
-    const [ imgTitle, setImgtitle] = useState(null);
-    const [ thumbImg, setThumbImg] = useState(null);
+    const [ imgTitle, setImgTitle] = useState(null);
+    const [ imgSm, setImgSm] = useState(null);
     const [ thriller, setThriller] = useState(null);
     const [ video, setVideo] = useState(null);
+    const [ upload, setUpload] = useState(0);
 
     const handleChange = (e) => {
         const value = e.target.value;
         setMovie({...movie, [e.target.name]: value});
     }
+   
+    const uploading = (items) => {
+        items.forEach((item) => {
+            const uploadTask = storage.ref(`/items/${item.file.name}`).put(item);
+            uploadTask.on("state_changes", (snapshot) => {
+                const progress = (snapshot.byteTransfered / snapshot.totaBytes) * 100;
+                console.log("loading" + progress + "% done");
+            },
+            (err) => {
+                console.log(err)
+            },
+            () => {
+                uploadTask.snapshot.ref.getDownloadURL().then((url) => {
+                    setMovie((prev) => {
+                        return {...prev, [item.label]: url };
+                    });
+                     setUpload((prev) => prev + 1);
+                });
+              }
+            );
+        });
+    }
+
+// handle all the files properties(input)
+    const handleUpload = (e) => {
+        e.preventDefault();
+        uploading ([
+            {file: img, label: "img"},
+            {file: imgTitle, label: "imgTitle"},
+            {file: imgSm, label: "imgSm"},
+            {file: thriller, label: "thriller"},
+            {file: video, label: "video"},
+        ])
+    }
+
     console.log(movie)
+
      return (
         <div className="createproduct">
              <h1 className="addProdctTitle">New Movies</h1>
@@ -25,11 +63,11 @@ const CreateProducts = () => {
                 </div>
                 <div className="addProductItem">
                     <label>Title</label>
-                    <input type ="file" style={{cursor: "pointer"}} onChange={e=>setImgtitle(e.target.files[1])}  id="imgTitle" name="imgTitle" />
+                    <input type ="file" style={{cursor: "pointer"}} onChange={e=>setImgTitle(e.target.files[0])}  id="imgTitle" name="imgTitle" />
                 </div>
                 <div className="addProductItem">
                     <label>Thumbnail image</label>
-                    <input type ="file" style={{cursor: "pointer"}} onChange={e=>setThumbImg(e.target.files[2])}  id="thumbImg" name="thumbImg" />
+                    <input type ="file" style={{cursor: "pointer"}} onChange={e=>setImgSm(e.target.files[0])}  id="thumbImg" name="imgSm" />
                 </div>
                 <div className="addProductItem">
                     <label>Title</label>
@@ -57,11 +95,11 @@ const CreateProducts = () => {
                 </div>
                 <div className="addProductItem">
                     <label>Thriller</label>
-                    <input type="file" style={{cursor: "pointer"}} onChange={handleChange}  placeholder="thriller" name="thriller" />
+                    <input type="file" style={{cursor: "pointer"}} onChange={e=>setThriller(e.target.files[0])}  placeholder="thriller" name="thriller" />
                 </div>
                 <div className="addProductItem">
                     <label>Video</label>
-                    <input type="file" style={{cursor: "pointer"}} onChange={e=>setVideo(e.target.files[2])}  placeholder="video" name="video" />
+                    <input type="file" style={{cursor: "pointer"}} onChange={e=>setVideo(e.target.files[0])}  placeholder="video" name="video" />
                 </div>
                 <div className="addProductItem">
                     <label>Series?</label>
@@ -69,7 +107,11 @@ const CreateProducts = () => {
                         <option value="false">no</option>
                         <option value="true">yes</option>
                     </select>
-                    <button className="addProductBtn">Create</button>
+                    {upload ===5 ? (
+                            <button className="addProductBtn">Create</button>
+                        ) : (
+                            <button className="addProductBtn" onClick={handleUpload}>Upload</button>
+                        )}
                 </div>
             </form>
         </div>
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase"
+
+
+firebaseConfig = {
+    apiKey: "AIzaSyDPRGqK7RZZTmjnrDc1nYz9xN7MzoUTCKU",
+    authDomain: "movieapp-659f3.firebaseapp.com",
+    projectId: "movieapp-659f3",
+    storageBucket: "movieapp-659f3.appspot.com",
+    messagingSenderId: "619387833366",
+    appId: "1:619387833366:web:dcaef3d0f57259b2c1b937",
+    measurementId: "G-PKKHSXXR52"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  const storage = firebase.storage();
+
+  export default storage;
